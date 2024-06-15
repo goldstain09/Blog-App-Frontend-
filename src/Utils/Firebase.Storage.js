a904e38f04b48af74f762dc04df19de3435544f7
Sujal Rajput <rajputsujal992@gmail.com>
2024-06-15 05:40:05 +0530

    Update Firebase.Storage.js
    updating my firebase.storage.js to securing my firebase configs...

--- src/Utils/Firebase.Storage.js
+++ src/Utils/Firebase.Storage.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDOUhyTb9DdM2lPe86JlWyAfTOF8MOn9Y",
+  apiKey: "AIzaSyDDOUhyTb9DdTTM2lPe86JlWyAfTOF8MOn9Y",
   authDomain: "blog-app-2d912.firebaseapp.com",
   projectId: "blog-app-2d912",
   storageBucket: "blog-app-2d912.appspot.com",
-  messagingSenderId: "511481982451",
-  appId: "1:511481982451:web:c69d5d67b72c1d62e43686",
-  measurementId: "G-YR3LVCHTY6"
+  messagingSenderId: "51148191182451",
+  appId: "1:511481982451:web:c69d5d67b72c1TTd62e43686",
+  measurementId: "G-YR3LTTVCHTY6"
 };
 
 
